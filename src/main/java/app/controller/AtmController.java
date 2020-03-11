125b289d23ad0a254472545881999b19a979d1ad
greepz <trunov.a.serg@sberbank.ru>
2020-03-11 19:58:09 +0300

    add account controller

--- src/main/java/app/controller/AtmController.java
+++ src/main/java/app/controller/AtmController.java
@@ -2,24 +2,48 @@ package app.controller;
 
 import app.Store;
 import app.model.Account;
+import app.service.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class AtmController {
 
-    @GetMapping("/balance")
-    public String getBalance(){
-        return "index";
+    @Autowired
+    @Qualifier("StoreServiceImpl")
+    private StoreService service;
+
+//    @GetMapping("/balance")
+//    public String getBalance(){
+//        return "index";
+//    }
+//
+//    @GetMapping("/users")
+//    public ResponseEntity<Map<String, Account>> getAccounts(){
+//        return new ResponseEntity<>(Store.getStore(), HttpStatus.OK);
+//    }
+
+    // GET http://localhost:9011/account?id=100
+
+    @GetMapping("/account")
+    public ResponseEntity<Account> getAccount(@RequestParam("id") String id){
+        Account acc = service.get(id);
+        return new ResponseEntity<>(acc, HttpStatus.OK);
     }
 
-    @GetMapping("/users")
-    public ResponseEntity<Map<String, Account>> getAccounts(){
-        return new ResponseEntity<>(Store.getStore(), HttpStatus.OK);
+    @GetMapping("/accounts")
+    public ResponseEntity<List<Account>> getAccounts(){
+        List<Account> accounts = service.get();
+        return new ResponseEntity<>(accounts, HttpStatus.OK);
     }
+
 
 }
